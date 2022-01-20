1864931a2c547265b0848656f8c784bc6a318b83
Vuong Hoang Hieu <vuonghoanghieu96@gmail.com>
2022-01-20 07:55:32 +0700

    Fix"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ModalPhimAnhComponent } from './map-modal/modal-phim-anh/modal-phim-anh.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { JwtInterceptorService } from './_helpers/jwt-interceptor.service';
-
-@NgModule({
-  declarations: [AppComponent, ModalPhimAnhComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: JwtInterceptorService,
-      multi: true,
-    },
-  ],
-=======
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -40,7 +23,6 @@ import { PhimAnhPopupComponent } from './map/map-popup/phim-anh-popup/phim-anh-p
     MatButtonModule,
   ],
   providers: [],
->>>>>>> 582460cc42b03e0e4e86685ea1fd0b3a07ed4354
   bootstrap: [AppComponent],
 })
 export class AppModule {}
